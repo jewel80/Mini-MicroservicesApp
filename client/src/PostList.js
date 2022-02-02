b056aa7b798977ffbeadffcdbd5b1bc76f40a08b
Md. Jewel Rana <m.jewelrana80@gmail.com>
2022-02-02 19:26:48 +0600

    creating the mordarate services

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -7,8 +7,8 @@ const PostList = () => {
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
-    const res = await axios.get("http://localhost:4000/posts");
-
+    const res = await axios.get("http://localhost:4002/posts");
+    console.log(res.data);
     setPosts(res.data);
   };
 
@@ -26,28 +26,12 @@ const PostList = () => {
       >
         <div className="card-body">
           <h3>{post.title}</h3>
-          <CommentList postId={post.id} />
+          <CommentList comments={post.comments} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
     );
   });
-
-  // const renderedPosts = Object.values(posts).map((post) => {
-  //   return (
-  //     <div
-  //       className="card"
-  //       style={{ width: "30%", marginBottom: "20px" }}
-  //       key={post.id}
-  //     >
-  //       <div className="card-body">
-  //         <h3>{post.title}</h3>
-  //         <CommentList comments={post.comments} />
-  //         <CommentCreate postId={post.id} />
-  //       </div>
-  //     </div>
-  //   );
-  // });
 
   return (
     <div className="d-flex flex-row flex-wrap justify-content-between">
